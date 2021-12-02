ba9c8f34bdd6a53c96ee2c788c87645c104150ae
kyler-swanson <kylerswanson@gmail.com>
2021-12-02 14:32:39 -0500

    Add washroom log endpoint

--- api/routes/api.js
+++ api/routes/api.js
@@ -2,12 +2,13 @@ var express = require("express");
 var router = express.Router();
 
 const WashroomStatus = require('../models/WashroomStatus.js');
+const WashroomLog = require('../models/WashroomLog.js');
 
 router.get("/", function(req, res, next) {
     res.send("Welcome to the WashroomWhistle API!");
 });
 
-router.get("/status/:deviceId", async function(req, res, next) {
+router.get("/status/:deviceId", async (req, res, next) => {
     const deviceId = req.params.deviceId;
     const deviceStatus = await WashroomStatus.findOne({ washroomId: deviceId }).exec();
 
@@ -23,5 +24,25 @@ router.get("/status/:deviceId", async function(req, res, next) {
         });
     }
 });
+
+router.get("/logs/:deviceId/:start?/:limit?", async (req, res, next) => {
+    const deviceId = req.params.deviceId;
+
+    const { skip = 0, limit = 0 } = req.params;
+
+    const logs = await WashroomLog.find({ washroomId: deviceId }, '-_id occupied timestamp', { skip: +skip, limit: +limit, sort: '-timestamp' }).exec();
+
+    if (logs != null && logs != undefined) {
+        res.json({
+            washroomId: deviceId,
+            events: logs
+        });
+    } else {
+        res.json({
+            washroomId: deviceId,
+            events: []
+        });
+    }
+}); 
 
 module.exports = router;
